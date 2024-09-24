b315b0f07a95fcdbc196b6e65f6eebe99b11ef83
baseplate77 <base8087@gmail.com>
2024-09-24 16:54:51 +0530

    ff

--- app/index.ts
+++ app/index.ts
@@ -82,44 +82,41 @@ app.get("/private-api", async (req: Request, res: Response) => {
     let inbox;
     let thereIsMore = false;
 
-    let f = await ig.user.info("111695513561042");
-    console.log("details :", f);
+    do {
+      inbox = await inboxFeed.items();
+      try {
+        inbox.forEach((thread) => {
+          thread.users.forEach(async (user) => {
+            // try {
+            //   // get complete user info
+            //   let userProfile = await ig.user.info(user.pk);
+            //   console.log("user :", user.username, JSON.stringify(userProfile));
 
-    // do {
-    //   inbox = await inboxFeed.items();
-    //   try {
-    //     inbox.forEach((thread) => {
-    //       thread.users.forEach(async (user) => {
-    //         // try {
-    //         //   // get complete user info
-    //         //   let userProfile = await ig.user.info(user.pk);
-    //         //   console.log("user :", user.username, JSON.stringify(userProfile));
+            //   console.log("********************");
+            //   // console.log("userProfile :", JSON.stringify(userProfile));
+            // } catch (error) {
+            //   console.log("error :", error);
+            // }
+            dmList.push(user);
+            console.log(
+              `User: ${user.username}, Full Name: ${user.full_name} `,
+              JSON.stringify(thread.last_activity_at),
+              JSON.stringify(thread.last_seen_at)
+              // `${thread.thread_id}`
+            );
+          });
+        });
 
-    //         //   console.log("********************");
-    //         //   // console.log("userProfile :", JSON.stringify(userProfile));
-    //         // } catch (error) {
-    //         //   console.log("error :", error);
-    //         // }
-    //         dmList.push(user);
-    //         console.log(
-    //           `User: ${user.username}, Full Name: ${user.full_name} `,
-    //           JSON.stringify(thread.last_activity_at),
-    //           JSON.stringify(thread.last_seen_at)
-    //           // `${thread.thread_id}`
-    //         );
-    //       });
-    //     });
+        thereIsMore = inboxFeed.isMoreAvailable();
+        await delay(2000);
+      } catch (error) {
+        console.log("error :", error);
 
-    //     thereIsMore = inboxFeed.isMoreAvailable();
-    //     await delay(2000);
-    //   } catch (error) {
-    //     console.log("error :", error);
+        thereIsMore = false;
+      }
 
-    //     thereIsMore = false;
-    //   }
-
-    //   console.log("count :", dmList.length);
-    // } while (thereIsMore);
+      console.log("count :", dmList.length);
+    } while (thereIsMore);
 
     res.send({ ok: "l", dmList });
   } catch (error) {
